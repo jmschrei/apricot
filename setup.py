7fcf5b9b0b547658133647e74d81be0e54242d0f
Jacob Schreiber <jmschreiber91@gmail.com>
2019-09-27 23:41:18 -0700

    v0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='apricot-select',
-    version='0.4.0rc1',
+    version='0.4.0',
     author='Jacob Schreiber',
     author_email='jmschreiber91@gmail.com',
     packages=['apricot'],
