5876c2faa3f2f4f3bafe51eb684985d225bf5de0
Jacob Schreiber <jmschreiber91@gmail.com>
2025-06-09 18:51:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license='LICENSE.txt',
     description='apricot is a package for submodular selection of representative sets for machine learning models.',
     install_requires=[
-        "numpy >= 1.14.2",
+        "numpy >= 1.14.2, <= 2.0.1",
         "scipy >= 1.0.0",
         "numba >= 0.43.0",
         "tqdm >= 4.24.0",
