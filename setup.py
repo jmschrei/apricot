3b12793cbf4648cc888bcfd6b584b59fccbd52aa
Alexandre Gramfort <agramfort@meta.com>
2025-11-13 01:58:42 +0100

    bump python version in CI (#46)
    * bump python version in CI
    
    * from nose to pytest
    
    * bump numpy and scipy versions

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='apricot-select',
-    version='0.6.1',
-    author='Jacob Schreiber',
-    author_email='jmschreiber91@gmail.com',
-    packages=['apricot', 'apricot/functions'],
-    url='http://pypi.python.org/pypi/apricot-select/',
-    license='LICENSE.txt',
-    description='apricot is a package for submodular selection of representative sets for machine learning models.',
+    name="apricot-select",
+    version="0.6.1",
+    author="Jacob Schreiber",
+    author_email="jmschreiber91@gmail.com",
+    packages=["apricot", "apricot/functions"],
+    url="http://pypi.python.org/pypi/apricot-select/",
+    license="LICENSE.txt",
+    description="apricot is a package for submodular selection of representative sets for machine learning models.",
     install_requires=[
-        "numpy >= 1.14.2, <= 2.0.1",
-        "scipy >= 1.0.0",
-        "numba >= 0.43.0",
-        "tqdm >= 4.24.0",
-        "nose"
+        "numpy >= 1.19.5",
+        "scipy >= 1.6.0",
+        "numba >= 0.53.0",
+        "tqdm >= 4.56.0",
+        "pytest",
     ],
 )
 
